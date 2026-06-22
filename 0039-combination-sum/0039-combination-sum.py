5b5b842b85b4ad8e9ca267b63a563cab718e3eb0
Vedant Vijay <59541263+vedantvijay@users.noreply.github.com>
2026-06-22 07:56:11 +0530

    Time: 4 ms (84.16%), Space: 19.5 MB (63.69%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.py
@@ -0,0 +1,22 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        new_list = []
+        i = 0
+        def check_all(i,target,new_list):
+            
+            if target == 0:
+                res.append(new_list.copy())
+                return 
+            if i >= len(candidates):
+                return 
+            if candidates[i] <= target:
+                new_list.append(candidates[i])
+                check_all(i, target - candidates[i], new_list)
+                new_list.pop()
+            check_all(i+1, target, new_list)
+            
+        check_all(i,target,new_list)
+        return res
+            
+            
